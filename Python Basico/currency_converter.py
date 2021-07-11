1aeef7fd494e5eba0addb4eeb954b15a1f700ce4
JuanMa <ingjuanmasuarez@gmail.com>
2021-07-11 15:05:58 -0500

    Etapa con condicionales del currency_converter

--- Python Basico/currency_converter.py
+++ Python Basico/currency_converter.py
@@ -1,5 +1,34 @@
-cop = int(input("¿How many colombian pesos do you have? "))
-usd = 3834.55
-dollars = cop / usd
-dollars = round(dollars, 2)
-print("Do you have $" + str(dollars) + " dollars")
+menu = """
+Bienvenido al conversor de monedas
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Elige una opción: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    cop = int(input('¿How many colombian pesos do you have? '))
+    usd = 3834.55
+    dollars = cop / usd
+    dollars = round(dollars, 2)
+    print('Do you have $' + str(dollars) + ' dollars')
+
+elif opcion == '2':
+    cop = int(input('¿How many argentine pesos do you have? '))
+    usd = 95.84
+    dollars = cop / usd
+    dollars = round(dollars, 2)
+    print('Do you have $' + str(dollars) + ' dollars')
+
+elif opcion == '3':
+    cop = int(input('¿How many mexican pesos do you have? '))
+    usd = 19.87
+    dollars = cop / usd
+    dollars = round(dollars, 2)
+    print('Do you have $' + str(dollars) + ' dollars')
+
+else:
+    print('Enter a correct value')
